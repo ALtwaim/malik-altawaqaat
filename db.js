1be371ba3517e129bc35e1e7aee7ec3de098bbe4
ahmed <twaimahmed@gmail.com>
2026-06-10 21:36:05 +0300

    Railway database

--- db.js
+++ db.js
@@ -1,17 +1,18 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '9009',
-    database: 'Football'});
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
+});
 
 db.connect((err) => {
     if (err) {
-        console.log('Connection Error');
         console.log(err);
     } else {
-        console.log('Connected To MySQL');
+        console.log('MySQL Connected');
     }
 });
 
